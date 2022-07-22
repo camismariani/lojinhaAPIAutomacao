19375df0f50a7b772ff4922bcfda68423e1681d5
camila <camilamariani.cm@gmail.com>
2022-07-22 17:52:04 -0300

    alterei o valor da varialvel para 0

--- src/test/java/modulos/produto/ProdutoTest.java
+++ src/test/java/modulos/produto/ProdutoTest.java
@@ -8,9 +8,9 @@ import org.junit.jupiter.api.Test;
 @DisplayName("Teste de API Rest do módulo de produto")
 public class ProdutoTest {
     @Test
-    @DisplayName("Validar os limites proibidos do valor do Produto")
+    @DisplayName("Validar os limites proibidos do valor do Produtos")
     public void testValidarLimitesProibidosValorProduto(){
-        double valor=0.01;
+        double valor=0;
 
         Assertions.assertTrue(valor > 0);
     }
