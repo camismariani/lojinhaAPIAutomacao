9fec06d402956fc9b562fc3da8ecaf3032227089
camila <camilamariani.cm@gmail.com>
2022-07-22 22:08:43 -0300

    Configurando os dados de acesso API Rest

--- src/test/java/modulos/produto/ProdutoTest.java
+++ src/test/java/modulos/produto/ProdutoTest.java
@@ -1,17 +1,26 @@
 package modulos.produto;
 
-import io.restassured.internal.common.assertion.Assertion;
-import org.junit.jupiter.api.Assertions;
+import io.restassured.RestAssured;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import static io.restassured.RestAssured.*;
+import static io.restassured.matcher.RestAssuredMatchers.*;
+import static org.hamcrest.Matchers.*;
+
 
 @DisplayName("Teste de API Rest do módulo de produto")
 public class ProdutoTest {
     @Test
-    @DisplayName("Validar os limites proibidos do valor do Produtos")
+    @DisplayName("Validar os limites proibidos do valor do Produto")
     public void testValidarLimitesProibidosValorProduto(){
-        double valor=0;
+        // COnfigurando os dados da API Rest da lojinha
+        baseURI = "http://165.227.93.41/";
+        // port = 8082;
+        basePath = "/lojinha";
 
-        Assertions.assertTrue(valor >= 0);
+        // obter o token do usuario admin
+
+        //tentar inserir um produto com valor 0.00 e validar que a msg de erro foi apresentada
+        // e o status code retornado foi 422
     }
 }
