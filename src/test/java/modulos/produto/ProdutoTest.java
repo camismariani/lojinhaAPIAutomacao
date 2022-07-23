a414283eea58f58bb82dfe63720478b4b17ba5dc
camila <camilamariani.cm@gmail.com>
2022-07-22 22:41:11 -0300

    validando a msg de erro apresentada e o status code retornado 422

--- src/test/java/modulos/produto/ProdutoTest.java
+++ src/test/java/modulos/produto/ProdutoTest.java
@@ -32,9 +32,30 @@ public class ProdutoTest {
                     .extract()
                         .path("data.token");
 
-        System.out.println(token);
-
         //tentar inserir um produto com valor 0.00 e validar que a msg de erro foi apresentada
         // e o status code retornado foi 422
+        given()
+            .contentType(ContentType.JSON)
+            .header("token",token)
+            .body("{\n" +
+                    "  \"produtoNome\": \"PS4\",\n" +
+                    "  \"produtoValor\": 0.00,\n" +
+                    "  \"produtoCores\": [\n" +
+                    "    \"branco\"\n" +
+                    "  ],\n" +
+                    "  \"produtoUrlMock\": \"\",\n" +
+                    "  \"componentes\": [\n" +
+                    "    {\n" +
+                    "      \"componenteNome\": \"controle\",\n" +
+                    "      \"componenteQuantidade\": 1\n" +
+                    "    }\n" +
+                    "  ]\n" +
+                    "}")
+        .when()
+            .post("/v2/produtos")
+        .then()
+            .assertThat()
+                .body("error", equalTo("O valor do produto deve estar entre R$ 0,01 e R$ 7.000,00"))
+                .statusCode(422);
     }
 }
